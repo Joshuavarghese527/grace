12f0562fdd8255b285169d536fd44a651a4c1a58
Joshua Varghese <joshuavarghese527@gmail.com>
2017-04-20 19:17:49 +0000

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,14 +68,10 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # config/environments/production.rb
-  config.paperclip_defaults = {
-storage: :s3,
-  s3_credentials: {
-    bucket: ENV['AWS_BUCKET'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+   config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aw_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aw_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
