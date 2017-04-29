b0827a7bb9e92f937491d432d5918b84807a3376
Joshua Varghese <joshuavarghese527@gmail.com>
2017-04-29 00:42:19 +0000

    Fixed paperclip bug

--- config/initializers/paperclip.rb
+++
@@ -1,2 +0,0 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 'https://console.aws.amazon.com/s3/buckets/grace-east-queens/?region=us-west-2&tab=overview'
